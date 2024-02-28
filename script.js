cc615689ae644ab562dfa80fa496ec6e56c23851
Melissa Pignalosa <m.pignalosa@hotmail.com>
2024-02-28 14:15:48 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//var favouriteColour = prompt("What is your favourite colour?");
+
+//console.log(`Your favourite colour is ${favouriteColour} .`);
+
+//var name = prompt("What is your name?");
+//var birthYear = prompt("What year were you born?");
+
+//console.log(` ${name} is ${2023 - birthYear} years old.`);
+
+var hourlyWage = prompt("What is your hourly wage?");
+var hoursWorked = prompt("How many hours do you work a week?");
+
+console.log(
+  `Jessica earns ${(hourlyWage * hoursWorked).toFixed(2)} in a week.`
+);
